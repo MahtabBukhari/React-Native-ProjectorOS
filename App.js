342c6e63cbc13e4a571836d199bdb9be32a4cf0b
MahtabBukhari <git config --global user.email you@example.com>
2023-05-18 07:40:11 -0500

    screens

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { icons } from './constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Pakistan</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
